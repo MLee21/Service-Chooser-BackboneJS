50cc59382a1bccd97695072b02046cfe6932c745
MLee21 <minnie.lee89@gmail.com>
2015-07-13 15:19:40 -0600

    return object so that chaining is possible.

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,8 @@ $(function(){
     render: function(){
       this.$el.html('<input type="checkbox" value="1" name="' + this.model.get('title') + '" /> ' + this.model.get('title') + '<span>$' + this.model.get('price') + '</span>');
       this.$('input').prop('checked', this.model.get('checked'));
+
+      return this;
     },
 
   });
