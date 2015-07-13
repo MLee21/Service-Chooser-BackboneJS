a43f3bb21889b80c3518541d3a234c2f55b009c8
MLee21 <minnie.lee89@gmail.com>
2015-07-13 15:10:16 -0600

    added services into servicelist collection.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,13 @@ $(function(){
     }
   });
 
+  var services = new ServiceList([
+    new Service({ title: 'web development', price: 200 }),
+    new Service({ title: 'web design', price: 250 }),
+    new Service({ title: 'photography', price: 100 }),
+    new Service({ title: 'coffee drinking', price: 10 })
+  ]);
+
 
 
 
