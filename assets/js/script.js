3e96cae87395a279644f31768b008e03c0af1c71
MLee21 <minnie.lee89@gmail.com>
2015-07-13 15:03:25 -0600

    added function that will return an array only with the checked services.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,11 @@ $(function(){
   });
 
   var ServiceList = Backbone.Collection.extend({
-    
+    model: Service,
+
+    getChecked: function() {
+      return this.where({checked:true});
+    }
   });
 
 
